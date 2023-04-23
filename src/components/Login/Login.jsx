8811ee471494f65a7b12b4004417d75561fa008c
Mustakim <sycospike1@gmail.com>
2023-04-23 11:08:14 +0600

    added navigate

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,11 @@
 import React, { useContext } from 'react';
 import './Login.css';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../providers/AuthProviders';
 
 const Login = () => {
     const {signIn} = useContext(AuthContext);
+    const navigate = useNavigate();
 
     const handleLogin = event => {
         event.preventDefault();
@@ -19,6 +20,7 @@ const Login = () => {
             const loggedUser = result.user ;
             console.log(loggedUser);
             form.reset();
+            navigate('/')
         })
         .catch(error => {
             console.log(error);
--- src/components/providers/AuthProviders.jsx
+++ src/components/providers/AuthProviders.jsx
@@ -9,6 +9,7 @@ const auth = getAuth(app);
 
 const AuthProviders = ({children}) => {
     const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     const createUser = (email, password) => {
         return createUserWithEmailAndPassword(auth, email, password);
@@ -26,6 +27,7 @@ const AuthProviders = ({children}) => {
     useEffect( ()=>{
        const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
+            setLoading(false);
         });
 
         // stop observing while unmounting
@@ -36,6 +38,7 @@ const AuthProviders = ({children}) => {
 
 const authInfo = {
 user,
+loading,
 createUser,
 signIn,
 logOut
